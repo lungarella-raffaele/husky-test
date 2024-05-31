7320f2a176fdb2552dd7467e29f63bb4cd61b209
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-05-31 10:45:25 +0200

    Added format and lint

--- src/lib/scritps/typescript.spec.ts
+++ src/lib/scritps/typescript.spec.ts
@@ -2,6 +2,6 @@ import { twoPlusTwo } from './typescript';
 import { test, expect } from 'vitest';
 
 test('testdiprova', () => {
-  const result = 3;
-  expect(twoPlusTwo()).toEqual(result);
-})
+	const result = 4;
+	expect(twoPlusTwo()).toEqual(result);
+});
